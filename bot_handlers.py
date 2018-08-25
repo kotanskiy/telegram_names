4f32b8f12fdf377ef8d913c188134c0463c69593
Alexey <olexiyyefimenko@gmail.com>
2018-08-25 13:08:19 +0300

    refactoring

--- bot_handlers.py
+++ bot_handlers.py
@@ -3,16 +3,19 @@ from bot_utils import *
 from bot import bot
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = ForceReply(selective=False)
-    greeting_message = generate_greeting_message()
-
+def add_name_to_greeting_message(message, greeting_message):
     try:
         user = User.objects(telegram_id=message.chat.id).get()
         greeting_message += ', ' + user.enter_name
+        return greeting_message
     except DoesNotExist:
-        pass
+        return greeting_message
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = ForceReply(selective=False)
+    greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     bot.send_message(message.chat.id, greeting_message)
     bot.send_message(message.chat.id, 'Введите свое имя:', reply_markup=markup)
 
@@ -20,12 +23,7 @@ def start(message):
 @bot.message_handler(func=lambda message: message.text == 'Изменить Имя ☝️')
 def rename_user(message):
     markup = ForceReply(selective=False)
-    greeting_message = generate_greeting_message()
-    try:
-        user = User.objects(telegram_id=message.chat.id).get()
-        greeting_message += ', ' + user.enter_name
-    except DoesNotExist:
-        pass
+    greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     keyboard = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
     button = KeyboardButton('Изменить Имя ☝️')
     keyboard.add(button)
@@ -38,11 +36,7 @@ def hello(message):
     greeting_message = update_or_save_user(message)
     if not greeting_message:
         greeting_message = generate_greeting_message()
-    try:
-        user = User.objects(telegram_id=message.chat.id).get()
-        greeting_message += ', ' + user.enter_name
-    except DoesNotExist:
-        pass
+    greeting_message = add_name_to_greeting_message(message, greeting_message)
     keyboard = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
     button = KeyboardButton('Изменить Имя ☝️')
     keyboard.add(button)
