bb7bbd3e7bb07703d024e865808e1316d479b39a
Alexey <olexiyyefimenko@gmail.com>
2018-08-26 01:15:29 +0300

    fix handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -6,17 +6,17 @@ from bot import bot
 def start(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     send_rename_button(message, greeting_message)
-    bot.send_message(message.chat.id, 'Введите свое имя:')
-    # bot.register_next_step_handler(msg, rename_user)
-    bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
+    msg = bot.send_message(message.chat.id, 'Введите свое имя:')
+    bot.register_next_step_handler(msg, rename_user)
+    # bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Изменить Имя ☝️')
 def enter_user_name(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     bot.send_message(message.chat.id, greeting_message)
-    bot.send_message(message.chat.id, 'Введите свое имя:')
-    bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
+    msg = bot.send_message(message.chat.id, 'Введите свое имя:')
+    bot.register_next_step_handler(msg, rename_user)
 
 
 @bot.message_handler(content_types=['text'])
@@ -25,5 +25,5 @@ def hello(message):
     bot.send_message(message.chat.id, greeting_message)
 
 
-bot.enable_save_next_step_handlers(delay=1)
+bot.enable_save_next_step_handlers()
 bot.load_next_step_handlers()
