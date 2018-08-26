dcae499ade1ab2c85fc76842a1a079ecfcdd8c56
Alexey <olexiyyefimenko@gmail.com>
2018-08-26 11:19:17 +0300

    fix handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -6,15 +6,16 @@ from bot import bot
 def start(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     send_rename_button(message, greeting_message)
-    bot.send_message(message.chat.id, 'Введите свое имя:')
-    bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
+    msg = bot.reply_to(message, 'Введите свое имя:')
+    bot.register_next_step_handler(msg, rename_user)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Изменить Имя ☝️')
 def enter_user_name(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
-    bot.send_message(message.chat.id, greeting_message + '\nВведите свое имя:')
-    bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
+    bot.send_message(message.chat.id, greeting_message)
+    msg = bot.reply_to(message, 'Введите свое имя:')
+    bot.register_next_step_handler(msg, rename_user)
 
 
 @bot.message_handler(content_types=['text'])
