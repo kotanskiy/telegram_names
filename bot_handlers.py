67763096e9e706206a5876dd89ea8bc7766e8673
Alexey <olexiyyefimenko@gmail.com>
2018-08-26 10:20:22 +0300

    fix handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -6,17 +6,17 @@ from bot import bot
 def start(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     send_rename_button(message, greeting_message)
-    msg = bot.send_message(message.chat.id, 'Введите свое имя:')
-    bot.register_next_step_handler(msg, rename_user)
+    bot.send_message(message.chat.id, 'Введите свое имя:')
+    bot.register_next_step_handler(message, rename_user)
     # bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Изменить Имя ☝️')
 def enter_user_name(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
-    # bot.send_message(message.chat.id, greeting_message)
-    msg = bot.send_message(message.chat.id, 'Введите свое имя:')
-    bot.register_next_step_handler(msg, rename_user)
+    bot.send_message(message.chat.id, greeting_message)
+    bot.send_message(message.chat.id, 'Введите свое имя:')
+    bot.register_next_step_handler(message, rename_user)
 
 
 @bot.message_handler(content_types=['text'])
@@ -25,6 +25,6 @@ def hello(message):
     bot.send_message(message.chat.id, greeting_message)
 
 
-bot.enable_save_next_step_handlers()
-bot.load_next_step_handlers()
+# bot.enable_save_next_step_handlers()
+# bot.load_next_step_handlers()
 
