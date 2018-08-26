e4c770d69d46a0c239b95d8aaa9704195238d62d
Alexey <olexiyyefimenko@gmail.com>
2018-08-26 11:03:45 +0300

    refactoring

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,4 +1,3 @@
-# from app import app
 from bot_utils import *
 from bot import bot
 
@@ -9,13 +8,11 @@ def start(message):
     send_rename_button(message, greeting_message)
     msg = bot.send_message(message.chat.id, 'Введите свое имя:')
     bot.register_next_step_handler(msg, rename_user)
-    # bot.register_next_step_handler_by_chat_id(message.chat.id, rename_user)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Изменить Имя ☝️')
 def enter_user_name(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
-    # bot.send_message(message.chat.id, greeting_message)
     msg = bot.send_message(message.chat.id, greeting_message + '\nВведите свое имя:')
     bot.register_next_step_handler(msg, rename_user)
 
@@ -25,9 +22,3 @@ def hello(message):
     greeting_message = add_name_to_greeting_message(message, generate_greeting_message())
     bot.send_message(message.chat.id, greeting_message)
 
-
-# bot.enable_save_next_step_handlers()
-# bot.load_next_step_handlers()
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
